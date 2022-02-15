6796fa6d51a14ccd921c4764e823694ae68c50a9
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-02-14 19:46:53 -0600

    utilización de prepros y el primer dibujo

---
+++ game-dist.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* console.log('Hola mundo'); */
+;
+
+(function () {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  ctx.fillRect(0, 0, 50, 50);
+})();
