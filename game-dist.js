e057ae62962299dce5fa1d919472e73854187fe3
Monse Vásquez <47286468+MonseV@users.noreply.github.com>
2022-02-22 15:29:28 -0600

    iniciando el snake

--- game-dist.js
+++ game-dist.js
@@ -1,10 +1,54 @@
 "use strict";
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+
 /* console.log('Hola mundo'); */
 ;
 
 (function () {
+  var Square = /*#__PURE__*/function () {
+    function Square(x, y) {
+      _classCallCheck(this, Square);
+
+      this.x = x;
+      this.y = y;
+    }
+
+    _createClass(Square, [{
+      key: "draw",
+      value: function draw() {
+        ctx.fillRect(this.x, this.y, 10, 10);
+      }
+    }]);
+
+    return Square;
+  }();
+
+  var Snake = /*#__PURE__*/function () {
+    function Snake() {
+      _classCallCheck(this, Snake);
+
+      /* this.draw() */
+      this.head = new Square(100, 0);
+      this.draw();
+    }
+
+    _createClass(Snake, [{
+      key: "draw",
+      value: function draw() {
+        /* ctx.fillRect(20,20,10,10) */
+        this.head.draw();
+      }
+    }]);
+
+    return Snake;
+  }();
+
   var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
-  ctx.fillRect(0, 0, 50, 50);
+  var snake = new Snake();
 })();
--- game.js
+++ game.js
@@ -1,8 +1,36 @@
 /* console.log('Hola mundo'); */
 
 ;(function(){
+
+  class Square{
+    constructor(x,y){
+      this.x = x
+      this.y = y
+    }
+
+    draw(){
+      ctx.fillRect(this.x,this.y,10,10);
+    }
+  }
+
+  class Snake{
+    constructor(){
+      /* this.draw() */
+      this.head = new Square(100,0);
+      this.draw()
+    }
+
+    draw(){
+      /* ctx.fillRect(20,20,10,10) */
+      this.head.draw();
+    }
+
+  }
+
   const canvas = document.getElementById('canvas')
   const ctx = canvas.getContext('2d')
 
-  ctx.fillRect(0,0,50,50);
+
+  const snake = new Snake()
+
 })()
